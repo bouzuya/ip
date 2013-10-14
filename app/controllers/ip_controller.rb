fc5fa52e539d244b283c096f7b8ee77b96bd2ae7
bouzuya <bouzuya@gmail.com>
2013-10-14 18:23:15 +0900

    add ip_controller

---
+++ app/controllers/ip_controller.rb
@@ -0,0 +1,4 @@
+class IpController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Ip::Application.routes.draw do
+  get "ip/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'ip#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
